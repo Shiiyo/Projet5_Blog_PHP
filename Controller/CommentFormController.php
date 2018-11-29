2f38b326d0926a95b1306dca918e8d66a0574fd7
Shiiyo <camaille13@hotmail.fr>
2018-11-29 17:26:52 +0100

    Create the form view in html and css

---
+++ Controller/CommentFormController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Controller;
+
+class CommentFormController implements ControllerInterface
+{
+    use ControllerTrait;
+
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
