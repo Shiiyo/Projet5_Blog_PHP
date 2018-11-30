21143966f3b45539f1a5646b547b674ec2c79546
Shiiyo <camaille13@hotmail.fr>
2018-11-30 12:00:10 +0100

    Start the logic code of the validation form, check every field and load the violations messages

--- Controller/CommentFormController.php
+++ Controller/CommentFormController.php
@@ -8,6 +8,30 @@ class CommentFormController implements ControllerInterface
 
     public function __invoke()
     {
-        // TODO: Implement __invoke() method.
+        $request = $this->getContainer()->newHttpRequest();
+
+        //Testing Recaptcha
+        $recaptchaResponse = $request->request->get('g-recaptcha-response');
+        $testRecaptcha = $this->getContainer()->newTestRecaptcha($this->getContainer(), $recaptchaResponse);
+        $verifyRecaptcha = $testRecaptcha();
+
+        //Testing form fields
+        $validator = $this->getContainer()->newValidator();
+        $validationForm = $this->getContainer()->newValidationForm($validator);
+
+        $violationName = $validationForm->validateName($request->request->get('nom'));
+        $violationEmail = $validationForm->validateEmail($request->request->get('email'));
+        $violationMessage = $validationForm->validateMessage($request->request->get('message'));
+        $violations = array($violationName, $violationEmail, $violationMessage);
+
+        //Get the error messages
+        $violationMessages = $this->getContainer()->newViolationMessages($violations, $verifyRecaptcha);
+        $error_msg = $violationMessages->violationMessages();
+
+        if ($error_msg == "") {
+        } else {
+            $this->session->set('error', $error_msg);
+            header('Location: /liste-blog-post');
+        }
     }
 }
