22d3f5087a473f68e2256ad948dccde0bd4da357
Shiiyo <camaille13@hotmail.fr>
2018-12-05 17:40:59 +0100

    Show a pop up with a confirmation or error message when you send the comment form

--- Controller/BlogPostController.php
+++ Controller/BlogPostController.php
@@ -28,5 +28,11 @@ class BlogPostController implements ControllerInterface
                                                                             'nbComment' => $nbComment]);
         $response = $this->getContainer()->newHttpResponseHtml($view);
         $response->send();
+
+        if ($this->session->get('success')!= null) {
+            $this->session->delete('success');
+        } elseif ($this->session->get('error')!= null) {
+            $this->session->delete('error');
+        }
     }
 }
