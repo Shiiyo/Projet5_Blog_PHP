2f1459b79d56471b9845948e01076113b0ef68b6
Shiiyo <camaille13@hotmail.fr>
2018-12-06 16:30:44 +0100

    Code the HTML and CSS file of the connection view

---
+++ Controller/Admin/AdminConnectionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Controller\Admin;
+
+use Controller\ControllerTrait;
+use Controller\ControllerInterface;
+
+class AdminConnectionController implements ControllerInterface
+{
+    use ControllerTrait;
+
+    public function __invoke()
+    {
+        $view = $this->getTwig()->render('admin/connection.html.twig');
+        $response = $this->getContainer()->newHttpResponseHtml($view);
+        $response->send();
+
+        if ($this->session->get('success')!= null) {
+            $this->session->delete('success');
+        } elseif ($this->session->get('error')!= null) {
+            $this->session->delete('error');
+        }
+    }
+}
