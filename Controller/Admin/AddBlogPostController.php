cae673517b762163ef50633fa371f04c26da2e64
Shiiyo <camaille13@hotmail.fr>
2018-12-18 17:34:12 +0100

    Create the design and the routeÃs

---
+++ Controller/Admin/AddBlogPostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controller\Admin;
+
+use Controller\ControllerInterface;
+use Controller\ControllerTrait;
+
+class AddBlogPostController implements ControllerInterface
+{
+    use ControllerTrait;
+
+    public function __invoke()
+    {
+        $testAdminLogIn = $this->container->newTestAdminLogIn();
+        $adminLogIn = $testAdminLogIn->testLogIn($this->session->get('uuid'), $this->container);
+
+        if ($adminLogIn != false) {
+            $view = $this->getTwig()->render('admin/addBlogPost.html.twig');
+            $response = $this->getContainer()->newHttpResponseHtml($view);
+            $response->send();
+        } else {
+            $this->session->delete('uuid');
+            $this->redirect('/admin/login');
+        }
+    }
+}
---
+++ Controller/Admin/PostNewBlogPost.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Controller\Admin;
+
+use Controller\ControllerInterface;
+use Controller\ControllerTrait;
+
+class PostNewBlogPost implements ControllerInterface
+{
+    use ControllerTrait;
+
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
